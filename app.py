875d250fc85af4be9a78fb8ec13b33ea0e5d0f46
aubed9 <71547236+aubed9@users.noreply.github.com>
2025-03-14 08:07:17 +0330

    Update app.py print5

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ BOT_TOKEN = "640108494:Y4Hr2wDc8hdMjMUZPJ5DqL7j8GfSwJIETGpwMH12"  # Store secure
 # Validate initData
 def validate_init_data(init_data):
     print(f"init_data type{type(init_data)}")
+    print(f"init_data: {init_data}")
     parsed_data = parse_qs(init_data)
     print(f"pars:{parsed_data}")
     print(f"parsed_data type: {parsed_data}")
