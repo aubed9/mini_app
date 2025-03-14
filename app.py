e72640a2a674794a811eaf0eed283f30e9093918
aubed9 <71547236+aubed9@users.noreply.github.com>
2025-03-14 08:56:19 +0330

    Update app.py print

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ login_manager.login_view = "login"
 
 # SQLite database setup
 def init_db():
-    conn = sqlite3.connect('data-base/users.db')
+    conn = sqlite3.connect('/data-base/users.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users 
                  (id INTEGER PRIMARY KEY AUTOINCREMENT, 
